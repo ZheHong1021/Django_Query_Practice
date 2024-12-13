893fe9ef28aea2cb3f4e58fd27c05ebc330e4737
zhe-hong-Lin <zhehome1021@gmail.com>
2024-12-13 15:28:48 +0800

    調整當登入取得TOKEN時，自動幫User更新last_login

--- config/settings/jwt.py
+++ config/settings/jwt.py
@@ -7,7 +7,7 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=3), # 指 Refresh Token 的有效期限，時效較【長】: user維持登入狀態的時間(通常建議將其設置為幾天到幾週之間。)
     "ROTATE_REFRESH_TOKENS": True, # (預設False):更新Token時，只會更新 access token。如果設為 True，則連 refresh token也會跟著更新
     "BLACKLIST_AFTER_ROTATION": True, # (預設False):設為True時，token更新之後，舊的 refresh 和 access token 都失去權限，這也相對安全
-    "UPDATE_LAST_LOGIN": False,
+    "UPDATE_LAST_LOGIN": True, # (預設False):設為True時，每次登入時，都會更新 user 的 last_login 欄位
 
     "ALGORITHM": "HS256",
     "SIGNING_KEY": SECRET_KEY,

721c6f11dc25f0bf59f3308506548dfec074c63a
zhe-hong-Lin <zhehome1021@gmail.com>
2024-12-17 17:21:55 +0800

    建立 common/models.py，儲存常用的abstract models

---
+++ common/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# 創建、修改日期的 Abstract Model
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True

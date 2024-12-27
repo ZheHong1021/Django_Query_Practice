dce8edee39cef317b90cd87c44d6fe77aa58cefe
zhe-hong-Lin <zhehome1021@gmail.com>
2024-12-27 14:52:01 +0800

    資料庫設定只用 .env

--- config/settings/databases.py
+++ config/settings/databases.py
@@ -14,26 +14,13 @@ from decouple import config  # 確保正確導入
 DATABASES = {
     'default': { # MySQL(預設)
         'ENGINE': 'django.db.backends.mysql',  
-        'NAME': 'query_practice', # MySQL 資料庫的名稱
-        'USER': 'myuser', # 使用者名稱
-        'PASSWORD': 'Ru,6e.4vu4wj/3', # 密碼
-        'HOST': 'db', # IP 地址
-        'PORT': '3306', # 埠號(mysql為 3306)
+        'NAME': config('MYSQL_DATABASE'), # MySQL 資料庫的名稱
+        'USER': config('MYSQL_USER'), # 使用者名稱
+        'PASSWORD': config('MYSQL_PASSWORD'), # 密碼
+        'HOST': config('DB_HOST', default='db'), # IP 地址
+        'PORT': config('DB_PORT', default='3306'), # 埠號(mysql為 3306)
         'OPTIONS': {
             'sql_mode': 'traditional',
         }
     },
 }
-# DATABASES = {
-#     'default': { # MySQL(預設)
-#         'ENGINE': 'django.db.backends.mysql',  
-#         'NAME': config('MYSQL_DATABASE'), # MySQL 資料庫的名稱
-#         'USER': config('MYSQL_USER'), # 使用者名稱
-#         'PASSWORD': config('MYSQL_PASSWORD'), # 密碼
-#         'HOST': config('DB_HOST', default='db'), # IP 地址
-#         'PORT': config('DB_PORT', default='3306'), # 埠號(mysql為 3306)
-#         'OPTIONS': {
-#             'sql_mode': 'traditional',
-#         }
-#     },
-# }

becb7a7c0cb7e94955570c2d2d6a199885686d06
zhe-hong-Lin <zhehome1021@gmail.com>
2024-12-27 14:51:21 +0800

    添加靜態檔案的urls

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from config.django.base import DEBUG
-
-
+from django.conf import settings
+from django.conf.urls.static import static
+import debug_toolbar
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 # All
 urlpatterns = [
@@ -26,25 +28,31 @@ urlpatterns = [
     path('api/', include('auth.user.urls')),
     path('api/', include('auth.jwt_token.urls')),
     path('api/', include('apps.posts.urls')),
+
+
+    # Swagger-UI
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
 ]
 
 # Development Mode
 if DEBUG:
-    import debug_toolbar
-    from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
     urlpatterns += [
-
         # debug_toolbar
         path("__debug__/", include(debug_toolbar.urls)),
-
-        # Swagger-UI
-        path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-        path("api/schema/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
+        
     ]
+
+    # Static and Media
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Production Mode
 else:
-    
     urlpatterns += [
+
     ]
+
+
+    

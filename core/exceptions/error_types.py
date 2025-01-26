db553d0fc80ac564cfe7ff508ae450e467c34420
ZheHong1021 <zhehome1021@gmail.com>
2025-01-27 01:05:51 +0800

    添加錯誤訊息(404)

--- core/exceptions/error_types.py
+++ core/exceptions/error_types.py
@@ -5,10 +5,10 @@ ERROR_TYPE_MESSAGES = {
     'InvalidToken': _('無效的Token，請重新登入'),
     'ValidationError': _('資料驗證失敗'),
     'NotAuthenticated': _('請先進行登入'),
-    'AuthenticationFailed': _('身份驗證失敗'),
+    'AuthenticationFailed': _('登入失敗'),
     'PermissionDenied': _('您沒有權限執行此操作'),
-    'NotFound': _('找不到請求的資源'),
-    'MethodNotAllowed': _('不允許的請求方法'),
+    'NotFound': _('找不到請求的資料'),
+    'MethodNotAllowed': _('不允許的請求方法 ({})'),
     'Throttled': _('請求次數過多，請稍後再試'),
     
     # 資料庫相關
@@ -16,11 +16,11 @@ ERROR_TYPE_MESSAGES = {
     'IntegrityError': _('資料完整性錯誤'),
     
     # HTTP 相關
-    'Http404': _('找不到請求的資源'),
+    'Http404': _('找不到請求的資料'),
     
     # 自定義業務錯誤
     'BusinessError': _('業務邏輯錯誤'),
-    'ResourceNotFoundError': _('找不到請求的資源'),
+    'ResourceNotFoundError': _('找不到請求的資料'),
     'InvalidOperationError': _('無效的操作'),
     
     # 預設錯誤

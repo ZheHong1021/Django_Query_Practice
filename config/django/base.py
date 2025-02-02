5d84789a15f43f6e6eecc404129d575c75a086c5
ZheHong1021 <zhehome1021@gmail.com>
2025-02-02 15:33:52 +0800

    調整時區為台灣

--- config/django/base.py
+++ config/django/base.py
@@ -119,9 +119,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hant' # 繁體中文
 
-TIME_ZONE = 'Asia/Taipei'
+TIME_ZONE = 'Asia/Taipei' # 台北時區
 
 USE_I18N = True
 

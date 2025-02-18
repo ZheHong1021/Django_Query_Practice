9c8b50fc2ae46537a90e7f29fbf157173cf5e285
ZheHong1021 <zhehome1021@gmail.com>
2025-02-18 16:45:20 +0800

    添加post app

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from common.models import TimeStampedModel, CreatedByModel
-from datetime import datetime
+from common.models import TimeStampedModel, CreatedByModel, UpdatedByModel, ImageModel, BaseUUIDModel
+from datetime import datetime, date
 
-class Post(TimeStampedModel, CreatedByModel):
+class Post(TimeStampedModel, CreatedByModel, UpdatedByModel):
     title = models.CharField("標題", max_length=100)
     content = models.TextField("內容")
-    record_date = models.DateField("貼文日期", blank=True, default=datetime.today)
+    record_date = models.DateField("貼文日期", blank=True, default=date.today)
 
     def __str__(self):
         return self.title
@@ -16,3 +16,14 @@ class Post(TimeStampedModel, CreatedByModel):
 
         verbose_name = '貼文訊息'  # 這會影響權限名稱
         verbose_name_plural = '貼文訊息列表'
+
+class PostImage(BaseUUIDModel, TimeStampedModel, CreatedByModel, UpdatedByModel, ImageModel):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="images")
+    class Meta:
+        db_table = "post_image"
+        ordering = ["-created_at"]
+
+        verbose_name = '貼文圖片'  # 這會影響權限名稱
+        verbose_name_plural = '貼文圖片列表'
+    
+    
